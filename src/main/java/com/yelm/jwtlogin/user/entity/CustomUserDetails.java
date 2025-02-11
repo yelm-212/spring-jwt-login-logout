37bda71e2b56328c13e33f02543a92341d82bf1a
yelm-212 <21yrshin@naver.com>
2025-02-11 13:05:16 +0900

    refactor: Deleted unused imports

--- src/main/java/com/yelm/jwtlogin/user/entity/CustomUserDetails.java
+++ src/main/java/com/yelm/jwtlogin/user/entity/CustomUserDetails.java
@@ -6,7 +6,6 @@ import org.springframework.security.core.userdetails.UserDetails;
 
 import java.util.ArrayList;
 import java.util.Collection;
-import java.util.List;
 
 @AllArgsConstructor
 public class CustomUserDetails implements UserDetails {
