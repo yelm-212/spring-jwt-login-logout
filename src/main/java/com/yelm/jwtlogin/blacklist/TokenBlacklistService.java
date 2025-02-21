82203113ba5250824257be9125a511406d62f4e8
yelm-212 <21yrshin@naver.com>
2025-02-21 14:45:06 +0900

    feat: inmemory logout blacklist

--- src/main/java/com/yelm/jwtlogin/blacklist/TokenBlacklistService.java
+++ src/main/java/com/yelm/jwtlogin/blacklist/TokenBlacklistService.java
@@ -1,32 +1,43 @@
 package com.yelm.jwtlogin.blacklist;
 
+import com.github.benmanes.caffeine.cache.Cache;
+import com.github.benmanes.caffeine.cache.Caffeine;
+import com.github.benmanes.caffeine.cache.RemovalCause;
 import com.yelm.jwtlogin.jwt.JWTUtil;
 import lombok.RequiredArgsConstructor;
-import org.springframework.data.redis.core.StringRedisTemplate;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
 
+import java.time.Duration;
 import java.util.concurrent.TimeUnit;
 
+@Slf4j
 @Service
 @RequiredArgsConstructor
 public class TokenBlacklistService {
-    private final StringRedisTemplate redisTemplate;
     private final JWTUtil jwtUtil;
+    private final Cache<String, String> tokenBlacklist = Caffeine.newBuilder()
+            .expireAfterWrite(60, TimeUnit.MINUTES) // 기본 TTL (fallback)
+            .removalListener((String key, String value, RemovalCause cause) ->
+                    log.debug("Token Expired: {} Cause: {}", key, cause))
+            .build();
+
 
     public void blacklistToken(String token) {
-        // 토큰의 남은 만료 시간 계산
         long ttl = jwtUtil.getExpirationTime(token) - System.currentTimeMillis();
 
         if (ttl > 0) {
-            // Redis에 블랙리스트 추가 (Key: token, Value: "blacklisted")
-            redisTemplate.opsForValue()
-                    .set("blacklist:" + token, "blacklisted", ttl, TimeUnit.MILLISECONDS);
+            tokenBlacklist.policy().expireVariably().ifPresent(policy -> {
+                policy.put(token, "blacklisted", Duration.ofMillis(ttl));
+            });
+
+            log.debug("Token Blacklisted: {}, TTL: {}ms", token, ttl);
+        } else {
+            log.debug("Token Expired: {}ms", ttl);
         }
     }
 
     public boolean isTokenBlacklisted(String token) {
-        return Boolean.TRUE.equals(
-                redisTemplate.hasKey("blacklist:" + token)
-        );
+        return tokenBlacklist.getIfPresent(token) != null;
     }
 }
